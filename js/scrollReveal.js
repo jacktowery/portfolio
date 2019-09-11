895b802fed34e0415dfda82b0335677087409938
Jack Towery <jacktowery@gmail.com>
2019-09-11 18:42:27 -0400

    Added new header animation

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,5 +1,26 @@
-ScrollReveal().reveal('.r-spin-grow-1',{duration: 600, rotate: {z: 20}, scale: 0.6});
+ScrollReveal().reveal('.r-spin-grow-1', {
+    duration: 600,
+    rotate: {
+        z: 14
+    },
+    scale: 0.6
+});
 
-ScrollReveal().reveal('.r-delayset-1',{ delay: 200, duration: 600, distance: '16px', origin: 'top' });
-ScrollReveal().reveal('.r-delayset-2',{ delay: 400, duration: 600, distance: '16px', origin: 'top' });
-ScrollReveal().reveal('.r-delayset-3',{ delay: 600, duration: 600, distance: '16px', origin: 'top' });
+ScrollReveal().reveal('.r-delayset-1', {
+    delay: 200,
+    duration: 600,
+    distance: '16px',
+    origin: 'top'
+});
+ScrollReveal().reveal('.r-delayset-2', {
+    delay: 400,
+    duration: 600,
+    distance: '16px',
+    origin: 'top'
+});
+ScrollReveal().reveal('.r-delayset-3', {
+    delay: 600,
+    duration: 600,
+    distance: '16px',
+    origin: 'top'
+});
