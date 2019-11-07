835fe7bb27967887f0f85520f31be3ad38dcac7c
Jack Towery <jacktowery@gmail.com>
2019-11-07 10:35:38 -0500

    New color, revised project cover, fixed animations
    *Changed primary color to purple
    *Updated layout of project covers
    *Fixed header logo animation on Home
    *Fixed skill bars animation on mobile

--- js/scrollReveal.js
+++ js/scrollReveal.js
@@ -1,7 +1,8 @@
 ScrollReveal().reveal('.r-spin-grow-1', {
     duration: 600,
+    delay: 200,
     rotate: {
-        z: 14
+        z: 20
     },
     scale: 0.6
 });
@@ -69,18 +70,82 @@ var skills = [
     ["Microsoft Office Suite", 75]
 ]
 
-function fillProgress() {
-    let bars = document.getElementsByClassName('progress-wrapper');
-    for (var i = 0; i < bars.length; i++) {
-        bars[i].querySelector('progress').value = skills[i][1];
-        bars[i].querySelector('p').innerText = skills[i][0];
+var skills1 = [
+    ["Adobe Illustrator", 90],
+    ["Adobe Photoshop", 55],
+    ["Adobe InDesign", 70],
+    ["Adobe Lightroom", 80],
+    ["Adobe AfterEffects", 45],
+    ["Adobe Premiere Pro", 50],
+    ["Paint.NET", 70],
+    ["Inkscape", 65]
+]
+
+var skills2 = [
+    ["Swift (Xcode)", 45],
+    ["Java (Android)", 55],
+    ["HTML", 90],
+    ["CSS/SASS", 85],
+    ["JavaScript", 65],
+    ["PHP", 35],
+    ["MySQL", 40],
+    ["Vue.js", 15]
+]
+
+var skills3 = [
+    ["MATLAB", 75],
+    ["R", 15],
+    ["Julia", 40],
+    ["Python", 30],
+    ["C++", 50],
+    ["Java", 70],
+    ["LaTeX", 85],
+    ["Microsoft Office Suite", 75]
+]
+
+
+function fillProgress(set) {
+    if (set == 1) {
+        let bars = document.querySelectorAll('#bars-1 .progress-wrapper');
+        for (var i = 0; i < bars.length; i++) {
+            bars[i].querySelector('progress').value = skills1[i][1];
+            bars[i].querySelector('p').innerText = skills1[i][0];
+        }
+    } else if (set == 2) {
+        let bars = document.querySelectorAll('#bars-2 .progress-wrapper');
+        for (var i = 0; i < bars.length; i++) {
+            bars[i].querySelector('progress').value = skills2[i][1];
+            bars[i].querySelector('p').innerText = skills2[i][0];
+        }
+    } else if (set == 3) {
+        let bars = document.querySelectorAll('#bars-3 .progress-wrapper');
+        for (var i = 0; i < bars.length; i++) {
+            bars[i].querySelector('progress').value = skills3[i][1];
+            bars[i].querySelector('p').innerText = skills3[i][0];
+        }
     }
 }
 
-ScrollReveal().reveal('#first-bar', {
-    delay: 200,
+ScrollReveal().reveal('#bars-1', {
+    delay: 400,
     duration: 1,
     afterReveal: function(el) {
-        fillProgress();
+        fillProgress(1);
+    }
+});
+
+ScrollReveal().reveal('#bars-2', {
+    delay: 400,
+    duration: 1,
+    afterReveal: function(el) {
+        fillProgress(2);
+    }
+});
+
+ScrollReveal().reveal('#bars-3', {
+    delay: 400,
+    duration: 1,
+    afterReveal: function(el) {
+        fillProgress(3);
     }
 });
